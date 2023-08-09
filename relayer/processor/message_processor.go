7df301a9d3c2736b8521724463eb7ec95f0b763d
izyak <76203436+izyak@users.noreply.github.com>
2023-08-09 09:31:10 +0545

    feat: better logging for tx, provider, query, processor (#120)
    Co-authored-by: izyak <test@test.com>

--- relayer/processor/message_processor.go
+++ relayer/processor/message_processor.go
@@ -339,16 +339,14 @@ func (mp *messageProcessor) handleMsgUpdateClientForIcon(ctx context.Context, sr
 	latestConsensusHeight := dst.clientState.ConsensusHeight
 
 	if !src.latestHeader.IsCompleteBlock() {
-		mp.log.Debug("Src latest IbcHeader is not complete Block",
-			zap.Int64("Height", int64(src.latestHeader.Height())))
 		return nil
 	}
 
 	if src.latestHeader.Height() <= latestConsensusHeight.RevisionHeight {
 		mp.log.Debug("Src latest header is less then latest client State",
-			zap.String("Chainid ", src.info.ChainID),
-			zap.Int64("LatestHeader height", int64(src.latestHeader.Height())),
-			zap.Int64("Client State height", int64(latestConsensusHeight.RevisionHeight)))
+			zap.String("chain-id", src.info.ChainID),
+			zap.Int64("latest-header-height", int64(src.latestHeader.Height())),
+			zap.Int64("client-state-height", int64(latestConsensusHeight.RevisionHeight)))
 
 		return nil
 	}
@@ -599,7 +597,7 @@ func (mp *messageProcessor) sendSingleMessage(
 
 	msgType := tracker.msgType()
 
-	dst.log.Debug(fmt.Sprintf("Will broadcast %s message", msgType), zap.Object("msg", tracker))
+	// dst.log.Debug(fmt.Sprintf("Will broadcast %s message", msgType), zap.Object("msg", tracker))
 
 	// Set callback for packet messages so that we increment prometheus metrics on successful relays.
 	callbacks := []func(rtr *provider.RelayerTxResponse, err error){}
